d2aefca2d2c4cd442c618aed865c7933af445dbc
ewhaqh9909 <43231975+ewhaqh9909@users.noreply.github.com>
2018-11-15 16:09:09 +0900

    6

--- 08_1.c
+++ 08_1.c
@@ -2,17 +2,42 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
-char *proverb="ALL that glisters is not gold.";
-void setPointer(char **q)
+int addfunc(int a,int b)
 {
-	*q=proverb;
+	return a+b;
 }
-int main(void)
+int subfunc(int a,int b)
 {
-	char *p="zzz";
-	setPointer(&p);
-	printf("%s\n",p);
+	return a-b;
+}
+int mulfunc(int a,int b)
+{
+	return a*b;
+}
+int divfunc(int a,int b)
+{
+	return a/b;
+}
+
+int main(){
+	int i;
+	int j;
+	char k;
+	int (*calcfunc) (int,int);
 	
+	printf("input calculation :");
+	scanf("%d %c %d",&i,&k,&j);
+	switch(k)
+	{
+		case '+':calcfunc=addfunc;
+			break;
+		case '-':calcfunc=subfunc;
+		break;
+		case '*':calcfunc=mulfunc;
+		break;
+		case '/':calcfunc=divfunc;
+		break;
+	}
+	printf("result:%i\n", calcfunc(i,j));
 	return 0;
 }
